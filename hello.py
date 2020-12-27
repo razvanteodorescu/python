d41cbccefa0f43549d3cd2256870e2f6cd1f28b6
razvanteodorescu <teodorescu_razvan@yahoo.com>
2020-12-27 16:13:57 +0200

    a

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 print('Hello World!')
-print('What is your name') # ask for their name
+print('What is your name?') # ask for their name
 myName = input()
 print('It is good to meet you, ' + myName)
 print('The length of your name is :')
